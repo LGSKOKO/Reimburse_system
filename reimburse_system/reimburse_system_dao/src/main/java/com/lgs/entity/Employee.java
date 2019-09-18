dc9e4ecee58e8a0daf5e3f166a2ec163e6a06b3f
LGSKOKO <420422844@qq.com>
2019-09-18 11:07:44 +0800

    员工实体类

---
+++ reimburse_system/reimburse_system_dao/src/main/java/com/lgs/entity/Employee.java
@@ -0,0 +1,66 @@
+package com.lgs.entity;
+
+/**
+ * 员工实体类
+ */
+public class Employee {
+    private String sn;//编号
+
+    private String password;//密码
+
+    private String name;//名字
+
+    private String departmentSn;//部门编号
+
+    private String post;//职位
+
+    private Department department;//部门
+
+    public String getSn() {
+        return sn;
+    }
+
+    public void setSn(String sn) {
+        this.sn = sn;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartmentSn() {
+        return departmentSn;
+    }
+
+    public void setDepartmentSn(String departmentSn) {
+        this.departmentSn = departmentSn;
+    }
+
+    public String getPost() {
+        return post;
+    }
+
+    public void setPost(String post) {
+        this.post = post;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+}
