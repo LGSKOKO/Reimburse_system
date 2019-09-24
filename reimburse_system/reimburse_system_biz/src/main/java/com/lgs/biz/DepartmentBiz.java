7ae7972947f50469f356ab4ec6f9a50bf2311ee1
LGSKOKO <420422844@qq.com>
2019-09-24 20:20:00 +0800

    Department的业务层接口

--- reimburse_system/reimburse_system_biz/src/main/java/com/lgs/biz/DepartmentBiz.java
+++ reimburse_system/reimburse_system_biz/src/main/java/com/lgs/biz/DepartmentBiz.java
@@ -8,9 +8,14 @@ import java.util.List;
 //Department的业务层接口
 public interface DepartmentBiz {
 
+    //添加部门
     void add(Department department);
+    //修改部门
     void edit(Department department);
+    //删除部门
     void remove(String sn);
+    //获得单个部门
     Department get(String sn);
+    //获得所以部门
     List<Department> getAll();
 }
